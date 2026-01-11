fec3a76d66c039bb5b0cc8986ef007e1a3882a7c
Berk AR <berk.ar12@yandex.com>
2026-01-11 22:23:47 +0300

    modifier added

--- app/src/main/java/com/berkar/compose101/MainActivity.kt
+++ app/src/main/java/com/berkar/compose101/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,15 +15,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.berkar.compose101.ui.theme.Compose101Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MainScreen()
         }
@@ -31,8 +35,14 @@ class MainActivity : ComponentActivity() {
 fun MainScreen() {
     Column {
         Text(
+            modifier = Modifier
+                .background(color = Color.Black)
+                .padding(5.dp)
+                .clickable {
+                    println("\"Hello Android!\"  clicked")
+                },
             text = "Hello Android!",
-            color = Color.Blue,
+            color = Color.White,
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold
         )
