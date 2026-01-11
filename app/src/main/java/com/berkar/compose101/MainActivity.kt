0e2e9f636000523f485bc1e88a576a908172b9f8
Berk AR <berk.ar12@yandex.com>
2026-01-11 23:08:39 +0300

    fun CustomText added

--- app/src/main/java/com/berkar/compose101/MainActivity.kt
+++ app/src/main/java/com/berkar/compose101/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -34,26 +36,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     Column {
-        Text(
-            modifier = Modifier
-                .background(color = Color.Black)
-                .padding(5.dp)
-                .clickable {
-                    println("\"Hello Android!\"  clicked")
-                },
-            text = "Hello Android!",
-            color = Color.White,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
-        )
-
-        Text(
-            text = "Hello World!",
-            color = Color.Blue,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
-        )
+        CustomText(text = "Hello Android!")
+        CustomText(text = "Hello World!")
+        CustomText(text = "Hello Kotlin!")
     }
+}
+
+@Composable
+fun CustomText(text: String) {
+    Text(
+        modifier = Modifier
+            .background(color = Color.Black)
+            .padding(5.dp)
+            .clickable {
+                println("\"$text\"  clicked")
+            }
+            .fillMaxSize(0.5f),
+        text = text,
+        color = Color.White,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    )
 }
 
 @Preview(showBackground = true)
