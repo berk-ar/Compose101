1fe8381dff389c845ef21c7a8355226a33f033e4
Berk AR <berk.ar12@yandex.com>
2026-01-13 12:37:57 +0300

    verticalArrangement and horizontalAlignment added

--- app/src/main/java/com/berkar/compose101/MainActivity.kt
+++ app/src/main/java/com/berkar/compose101/MainActivity.kt
@@ -7,14 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -35,9 +39,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         CustomText(text = "Hello Android!")
+        Spacer(modifier = Modifier.padding(5.dp))
         CustomText(text = "Hello World!")
+        Spacer(modifier = Modifier.padding(5.dp))
         CustomText(text = "Hello Kotlin!")
     }
 }
@@ -51,7 +63,8 @@ fun CustomText(text: String) {
             .clickable {
                 println("\"$text\"  clicked")
             }
-            .fillMaxSize(0.5f),
+            .width(200.dp),
+        //.fillMaxSize(0.5f),
         text = text,
         color = Color.White,
         fontSize = 24.sp,
