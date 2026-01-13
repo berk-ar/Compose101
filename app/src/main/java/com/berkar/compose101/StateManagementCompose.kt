6f436097e7b3620ebb0857cbb1876bca8158ccf4
Berk AR <berk.ar12@yandex.com>
2026-01-13 15:19:29 +0300

    image added

--- app/src/main/java/com/berkar/compose101/StateManagementCompose.kt
+++ app/src/main/java/com/berkar/compose101/StateManagementCompose.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -17,6 +18,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.berkar.compose101.ui.theme.Compose101Theme
@@ -49,6 +54,20 @@ fun MainScreenStateManagement() {
             Button(onClick = { println("Button Clicked") }) {
                 Text(text = "Button")
             }
+
+            Spacer(modifier = Modifier.padding(5.dp))
+
+            Image(
+                bitmap = ImageBitmap.imageResource(id = R.drawable.kotlin),
+                contentDescription = null
+            )
+
+            Spacer(modifier = Modifier.padding(5.dp))
+
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = null
+            )
         }
     }
 }
